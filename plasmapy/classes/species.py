2a7e0b23e097e32b3ba07b37960e53af3a3f56b2
Dominik Stanczak <stanczakdominik@gmail.com>
2017-07-18 20:33:58 +0200

    Initial particle pusher commit

---
+++ plasmapy/classes/species.py
@@ -0,0 +1,105 @@
+"""
+Class representing a group of particles"""
+# coding=utf-8
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+from astropy.constants import c
+from astropy import units as u
+
+
+class Species:
+    """
+    Object representing a species of particles: ions, electrons, or simply
+    a group of particles with a particular initial velocity distribution.
+
+    Parameters
+    ----------
+    q : float
+        particle charge
+    m : float
+        particle mass
+    scaling : float
+        number of particles represented by each macroparticle
+    pusher : function 
+        particle push algorithm
+    name : str
+        name of group
+    """
+
+    def __init__(self, q, m, n, plasma, scaling=1, dt=np.inf, nt=np.inf, name="particles"):
+        self.q = q
+        assert self.q.si.unit == u.C
+        self.m = m
+        assert self.m.si.unit == u.kg
+        self.N = int(n)
+        self.scaling = scaling
+        self.eff_q = q * scaling
+        assert self.eff_q.si.unit == u.C
+        self.eff_m = m * scaling
+        assert self.eff_m.si.unit == u.kg
+
+        self.plasma = plasma
+        if np.isinf(dt) and np.isinf(nt):
+            raise ValueError("Both dt and nt are infinite.")
+
+        self.dt = dt
+        assert self.dt.si.unit == u.s
+        self.NT = nt
+        self.t = np.arange(nt) * dt
+
+        self.x = np.zeros((n, 3), dtype=float) * u.m
+        self.v = np.zeros((n, 3), dtype=float) * (u.m / u.s)
+        self.name = name
+
+        self.position_history = np.zeros((self.NT, *self.x.shape), dtype=float) * u.m
+        self.velocity_history = np.zeros((self.NT, *self.v.shape), dtype=float) * (u.m / u.s)
+        self.B_interpolator = RegularGridInterpolator(
+            (self.plasma.x.si.value,
+             self.plasma.y.si.value,
+             self.plasma.z.si.value),
+            self.plasma.magnetic_field.T.si.value,
+            method="linear",
+            bounds_error=True)
+
+    def interpolate_fields(self):
+        interpolated_b = self.B_interpolator(self.x.si.value) * u.T
+        interpolated_e = np.zeros(
+            interpolated_b.shape) * u.V / u.m  # TODO: remove this placeholder once we figure out what to do with
+        # local electric fields
+        return interpolated_b, interpolated_e
+
+    def boris_push(self, init=False):
+        dt = -self.dt / 2 if init else self.dt
+        b, e = self.interpolate_fields()
+
+        vminus = self.v + self.eff_q * e / self.eff_m * dt * 0.5
+
+        # rotate to add magnetic field
+        t = -b * self.eff_q / self.eff_m * dt * 0.5
+        s = 2 * t / (1 + t * t)
+
+        vprime = vminus + np.cross(vminus.si.value, t) * u.m / u.s
+        vplus = vminus + np.cross(vprime.si.value, s) * u.m / u.s
+        v_new = vplus + self.eff_q * e / self.eff_m * dt * 0.5
+
+        self.v = v_new
+        if not init:
+            self.x += self.v * dt
+
+    def run(self):
+        self.boris_push(init=True)
+        for i in range(self.NT):
+            self.boris_push()
+            self.position_history[i] = self.x
+            self.velocity_history[i] = self.v
+
+    def apply_bc(self):
+        self.particle_bc(self)
+
+    def __repr__(self, *args, **kwargs):
+        return f"Species(q={self.q:.4f},m={self.m:.4f},N={self.N},name=\"{self.name}\",NT={self.NT})"
+
+    def __str__(self):
+        return f"{self.N} {self.scaling:.2e}-{self.name} with q = {self.q:.2e}, m = {self.m:.2e}, " \
+               f"{self.saved_iterations} saved history " \
+               f"steps over {self.NT} iterations"
---
+++ plasmapy/classes/tests/test_Species.py
@@ -0,0 +1,71 @@
+from plasmapy.classes import Plasma, Species
+from astropy import units as u
+import pytest
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+from astropy.visualization import quantity_support
+from astropy.modeling import models, fitting
+
+quantity_support()
+
+
+@pytest.fixture()
+def uniform_magnetic_field():
+    x = np.linspace(-10, 10, 3) * u.m
+    test_plasma = Plasma(x, x, x)
+    magfieldstr = 1 * u.T
+    test_plasma.magnetic_field[2] = magfieldstr
+    return test_plasma
+
+
+def test_particle_uniform_magnetic():
+    """
+        Tests the particle stepper for a uniform magnetic field motion.
+    """
+    test_plasma = uniform_magnetic_field()
+
+    q = 1 * u.C
+    m = 1 * u.kg
+    s = Species(q, m, 1, test_plasma, dt=1e-2 * u.s, nt=int(1e4), name="test particle")
+
+    perp_speed = 0.01 * u.m / u.s
+    parallel_speed = 1e-5 * u.m / u.s
+    s.v[:, 1] = perp_speed
+
+    s.v[:, 2] = parallel_speed
+    estimated_gyrofreq = (s.q * test_plasma.magnetic_field_strength.mean() / s.m).to(1 / u.s)
+    expected_gyroradius = s.v[0, 1] / estimated_gyrofreq
+    estimated_gyroperiod = 2 * np.pi / estimated_gyrofreq
+
+    s.run()
+
+    x = s.position_history[:, 0, 0]
+    y = s.position_history[:, 0, 1]
+    z = s.position_history[:, 0, 2]
+
+    def plot():
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_title("")
+        ax.plot(x, y, z)
+        ax.set_xlabel("$x$ position")
+        ax.set_ylabel("$y$ position")
+        ax.set_zlabel("$z$ position")
+        plt.show()
+
+    p_init = models.Polynomial1D(degree=1)
+    fit_p = fitting.LinearLSQFitter()
+    p = fit_p(p_init, s.t, z)
+
+    def velocity_plots():
+        plt.scatter(s.t, z)
+        plt.plot(s.t, p(s.t))
+        plt.show()
+
+    assert np.allclose(z, p(s.t), atol=1e-4 * u.m), "z-velocity doesn't stay constant!"
+
+    estimated_gyroradius = (x.max() - x.min()) / 2
+    assert np.isclose(expected_gyroradius, estimated_gyroradius, atol=1e-4 * u.m), "Gyroradii don't match!"
+
+    # plot()
